b66cbd021612ed9e54fa623891a77bddd2b16795
JulianBatHorse <julian.falck01@gmail.com>
2019-11-26 15:01:49 +0100

    now also with popup-window

--- src/main/java/rdmjokes/Output.java
+++ src/main/java/rdmjokes/Output.java
@@ -1,11 +1,11 @@
 package rdmjokes;
 
-//import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
 
 public class Output{
     
     public void output(Joke joke){
-        //JOptionPane.showMessageDialog(null,joke.joke);
+        JOptionPane.showMessageDialog(null,joke.joke);
         System.out.println(joke.joke);
         
         
